cf068a739cc8fc79a6ac10692c2d6cdab44028af
ibarbech <ibarbech@alumnos.unex.es>
2018-05-06 15:06:02 +0200

    update bot

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ import json
 import numpy as np
 from datetime import date, timedelta, datetime
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as pyplot
 import os
 from threading import Timer
 import pickle
-APIUrls = {"86":"http://158.49.112.86:8080/","109":"http://158.49.112.109:8080/","87":"http://158.49.112.87:8080/"} #
-machines = {"486":"158.49.112.86","487":"158.49.112.87","4109":"158.49.112.109"}
+
+APIUrls = {"86": "http://158.49.112.86:8080/", "109": "http://158.49.112.109:8080/",
+           "87": "http://158.49.112.87:8080/"}  #
+machines = {"86": "158.49.112.86", "87": "158.49.112.87", "109": "158.49.112.109"}
 listOfError = []
 
-APIERRORCODE = 20
+API_ERROR_CODE = 20
 SENSOR_ERROR_CODE = 30
 MACHINE_ERROR_CODE = 40
 
@@ -27,8 +30,8 @@ bot = telebot.TeleBot(TOKEN)
 
 mainKeyBoard = telebot.types.ReplyKeyboardMarkup(row_width=2)
 # functions = ['/GetTemperatureStatus','/DectiveKeyboardOptions','/GetMoisureStatus','/ActiveKeyboardOptions','/GetMoisureStatus','/ProcessAllData', '/GetGraphics']
-keys = ['/DectiveKeyboardOptions','/ActiveKeyboardOptions','/ProcessAllData', '/GetGraphics','/unsubcribe','/subcribe','/ChangeTimeToCheck <Time in seconds>']
-
+keys = ['/DectiveKeyboardOptions', '/ActiveKeyboardOptions', '/ProcessAllData', '/GetGraphics', '/unsubcribe',
+        '/subcribe', '/ChangeTimeToCheck <Time in seconds>']
 
 key1 = telebot.types.KeyboardButton('/DectiveKeyboardOptions')
 key2 = telebot.types.KeyboardButton('/ProcessAllData')
@@ -40,27 +43,28 @@ markupHide = telebot.types.ForceReply(selective=False)
 
 init_text = "Hi.\n These are the functions that you can use."
 for x in keys:
-    init_text += "\n"+"\t"*4+x
+    init_text += "\n" + "\t" * 4 + x
 
 subcribe = {}
 try:
-	with open("idchats.txt", 'rb') as fichero:
-	    subcribe = pickle.load(fichero)
+    with open("idchats.txt", 'rb') as fichero:
+        subcribe = pickle.load(fichero)
 except Exception as e:
-	with open("idchats.txt", 'wb') as fichero:
-		pickle.dump(subcribe, fichero, 0)
+    with open("idchats.txt", 'wb') as fichero:
+        pickle.dump(subcribe, fichero, 0)
 time = 180
 
-def Error( code,  idSensor=None):
+
+def Error(code, idSensor=None):
     global bot
     print "Error " + str(code)
     # if idSensor is None:
-        # listOfError.append({"id": code, "tipo": code, "valor": -1})
+    # listOfError.append({"id": code, "tipo": code, "valor": -1})
     # else:
-        # listOfError.append({"id": code + int(idSensor), "tipo": code, "valor": -1})
+    # listOfError.append({"id": code + int(idSensor), "tipo": code, "valor": -1})
 
     if code in APIUrls.keys():
-        listOfError.append((code, {"id": code, "tipo": APIERRORCODE, "valor": -1}))
+        listOfError.append((code, {"id": code, "tipo": API_ERROR_CODE, "valor": -1}))
         for id, s in subcribe.iteritems():
             if s is True:
                 bot.send_message(id, "Error Servidor Api Caida")
@@ -68,12 +72,13 @@ def Error( code,  idSensor=None):
         listOfError.append({"id": code + int(idSensor), "tipo": code, "valor": -1})
         for id, s in subcribe.iteritems():
             if s is True:
-                bot.send_message(id, "Error Sensor " + str(idSensor) )
+                bot.send_message(id, "Error Sensor " + str(idSensor))
     elif code in machines.keys():
-        listOfError.append({"id": code, "tipo": MACHINE_ERROR_CODE, "valor": -1})
+        listOfError.append({"id": code + MACHINE_ERROR_CODE, "tipo": MACHINE_ERROR_CODE, "valor": -1})
         for id, s in subcribe.iteritems():
             if s is True:
                 bot.send_message(id, "Error Servidor Servidor " + str(code) + " Esta Caido")
+
 
 def uploadErrors():
     while len(listOfError) is not 0:
@@ -82,10 +87,10 @@ def uploadErrors():
         success = False
         for k, url in APIUrls.iteritems():
             try:
-                print "Intentando subir ",url , x
-                requests.post(url, data = json.dumps(x), timeout=1)
+                print "Intentando subir ", url, x
+                requests.post(url, data=json.dumps(x), timeout=1)
                 listOfError.pop(-1)
-                success=True
+                success = True
                 break
             except Exception as e:
                 print e
@@ -94,45 +99,48 @@ def uploadErrors():
         if success is False:
             break
 
+
 def typeToText(key):
     return {"1": "Temperatura ambiente",
-             "2": "Humedad ambiente",
-             "3": "Humedad de la tierra",
-             "4": "Luminosidad",
-             "5": "AEMET Fecha de actualización",
-             "6": "AEMET Localidad",
-             "7": "AEMET Provincia",
-             "8": "AEMET Temperatura máxima",
-             "9": "AEMET Temperatura mínima",
-             "10": "AEMET Estado del cielo",
-             "11": "AEMET Cota de nieve",
-             "12": "AEMET Viento",
-             "13": "AEMET Racha",
-             "14": "AEMET Temperatura horas",
-             "15": "AEMET Sensación térmica máxima",
-             "16": "AEMET Sensación térmica mínima",
-             "17": "AEMET Sensación térmica",
-             "18": "AEMET Humedad",
-             "19": "AEMET Humedad máxima",
-             "20": "AEMET Humedad mínima",
-             "21": "AEMET Temperatura máxima"
-             }[key]
+            "2": "Humedad ambiente",
+            "3": "Humedad de la tierra",
+            "4": "Luminosidad",
+            "5": "AEMET Fecha de actualización",
+            "6": "AEMET Localidad",
+            "7": "AEMET Provincia",
+            "8": "AEMET Temperatura máxima",
+            "9": "AEMET Temperatura mínima",
+            "10": "AEMET Estado del cielo",
+            "11": "AEMET Cota de nieve",
+            "12": "AEMET Viento",
+            "13": "AEMET Racha",
+            "14": "AEMET Temperatura horas",
+            "15": "AEMET Sensación térmica máxima",
+            "16": "AEMET Sensación térmica mínima",
+            "17": "AEMET Sensación térmica",
+            "18": "AEMET Humedad",
+            "19": "AEMET Humedad máxima",
+            "20": "AEMET Humedad mínima",
+            "21": "AEMET Temperatura máxima"
+            }[key]
+
 
 def monthToNum(shortMonth):
-    return{
-        'Jan' : '01',
-        'Feb' : '02',
-        'Mar' : '03',
-        'Apr' : '04',
-        'May' : '05',
-        'Jun' : '06',
-        'Jul' : '07',
-        'Aug' : '08',
-        'Sep' : '09',
-        'Oct' : '10',
-        'Nov' : '11',
-        'Dec' : '12'
-        }[shortMonth]
+    return {
+        'Jan': '01',
+        'Feb': '02',
+        'Mar': '03',
+        'Apr': '04',
+        'May': '05',
+        'Jun': '06',
+        'Jul': '07',
+        'Aug': '08',
+        'Sep': '09',
+        'Oct': '10',
+        'Nov': '11',
+        'Dec': '12'
+    }[shortMonth]
+
 
 def doRequest(idSensor=""):
     for k, url in APIUrls.iteritems():
@@ -143,7 +151,8 @@ def doRequest(idSensor=""):
             print "Error al obtener datos " + url
     return None
 
-def processDataAndSendGraphics(data, message,text=""):
+
+def processDataAndSendGraphics(data, message, text=""):
     global bot
     dictDays = {}
     for dato in data:
@@ -155,12 +164,12 @@ def processDataAndSendGraphics(data, message,text=""):
             dictDays[d].append(dato["valor"])
     for k, v in dictDays.iteritems():
         dictDays[k] = np.mean(v)
-    listDates=[datetime.strptime(x, "%Y-%m-%d").date() for x in dictDays.keys()]
+    listDates = [datetime.strptime(x, "%Y-%m-%d").date() for x in dictDays.keys()]
     d1 = min(listDates)
     # d2 = max(listDates)
     d2 = datetime.date(datetime.now())
     delta = d2 - d1
-    for i in range(delta.days+1):
+    for i in range(delta.days + 1):
         d = (d1 + timedelta(days=i)).__str__()
         if d not in dictDays.keys():
             dictDays[d] = 0
@@ -174,6 +183,7 @@ def processDataAndSendGraphics(data, message,text=""):
     pyplot.close()
     with open('/tmp/photo.png', 'rb') as photo:
         bot.send_photo(message.from_user.id, photo)
+
 
 def processData():
     # Get ids sensors
@@ -205,50 +215,30 @@ def processData():
     else:
         return "Error al conectar con la Api"
 
-@bot.message_handler(commands=['start','help'])
-def handle_start_help(message):
-    subcribe[message.from_user.id] = True
-    print subcribe
-    with open("idchats.txt", 'wb') as fichero:
-        pickle.dump(subcribe, fichero, 0)
-    bot.send_message(message.from_user.id, init_text)
-
-@bot.message_handler(commands=['GetTemperatureStatus'])
-def handle_GetTemperatureStatus(message):
-    bot.send_message(message.from_user.id, "Falta implementar")
-
-@bot.message_handler(commands=['GetMoisureStatus'])
-def handle_GetMoisureStatus(message):
-    bot.send_message(message.from_user.id, "Falta implementar")
-
-@bot.message_handler(commands=['ProcessAllData'])
-def handle_ProcessAllData(message):
-    bot.send_message(message.from_user.id, processData(), reply_markup=mainKeyBoard)
-
-@bot.message_handler(commands=['GetGraphics'])
-def handle_GetGraphics(message):
+def SendGraphics(message):
     # Get ids sensors
     sensors = doRequest()
     if sensors is not None:
-        sensors = sorted(sensors, key = lambda x: x["id"])
+        sensors = sorted(sensors, key=lambda x: x["id"])
         for sensor in sensors:
             bot.send_message(message.from_user.id, "Procesando datos con id: " + str(sensor["id"]))
             if sensor["id"] in [1, 2, 3, 4]:  # Procesar Sensores Jardin
                 data = doRequest(sensor["id"])
                 if data is not None:
-                    processDataAndSendGraphics(data, message,"Sensor: " + str(sensor["id"]))
+                    processDataAndSendGraphics(data, message, "Sensor: " + str(sensor["id"]))
                 else:
                     bot.send_message(message.from_user.id, "Error Api no responde ")
             elif sensor["id"] in [31, 32, 33, 34]:
                 data = doRequest(sensor["id"])
                 if data is not None:
-                    processDataAndSendGraphics(data, message, "Errores del Sensor: " + str(sensor["id"]-SENSOR_ERROR_CODE))
+                    processDataAndSendGraphics(data, message,
+                                               "Errores del Sensor: " + str(sensor["id"] - SENSOR_ERROR_CODE))
                 else:
                     bot.send_message(message.from_user.id, "Error Api no responde ")
             elif sensor["id"] in APIUrls.keys():
                 data = doRequest(sensor["id"])
                 if data is not None:
-                    processDataAndSendGraphics(data, message,"Errores de la API: " + str(sensor["id"]))
+                    processDataAndSendGraphics(data, message, "Errores de la API: " + str(sensor["id"]))
                 else:
                     bot.send_message(message.from_user.id, "Error Api no responde ")
             else:
@@ -257,27 +247,61 @@ def handle_GetGraphics(message):
     else:
         bot.send_message(message.from_user.id, "Error Api no responde ")
 
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+    if message.from_user.id not in subcribe.keys():
+        subcribe[message.from_user.id] = True
+        print subcribe
+        with open("idchats.txt", 'wb') as fichero:
+            pickle.dump(subcribe, fichero, 0)
+    bot.send_message(message.from_user.id, init_text)
+
+
+@bot.message_handler(commands=['GetTemperatureStatus'])
+def handle_GetTemperatureStatus(message):
+    bot.send_message(message.from_user.id, "Falta implementar")
+
+
+@bot.message_handler(commands=['GetMoisureStatus'])
+def handle_GetMoisureStatus(message):
+    bot.send_message(message.from_user.id, "Falta implementar")
+
+
+@bot.message_handler(commands=['ProcessAllData'])
+def handle_ProcessAllData(message):
+    bot.send_message(message.from_user.id, processData(), reply_markup=mainKeyBoard)
+
+
+@bot.message_handler(commands=['GetGraphics'])
+def handle_GetGraphics(message):
+    Timer(1, lambda: SendGraphics(message)).start()
+
+
 @bot.message_handler(commands=['ActiveKeyboardOptions'])
 def handle_ActiveKeyboardOptions(message):
     bot.send_message(message.from_user.id, "ActiveKeyboard", reply_markup=mainKeyBoard)
+
 
 @bot.message_handler(commands=['DectiveKeyboardOptions'])
 def handle_DectiveKeyboardOptions(message):
     bot.send_message(message.from_user.id, "DectiveKeyboard", reply_markup=markupHide)
 
+
 @bot.message_handler(commands=['unsubcribe'])
 def handle_unsubcribe(message):
-    subcribe[message.from_user.id]=False
+    subcribe[message.from_user.id] = False
     with open("idchats.txt", 'wb') as fichero:
-    	pickle.dump(subcribe, fichero,0)
+        pickle.dump(subcribe, fichero, 0)
+
 
 @bot.message_handler(commands=['subcribe'])
 def handle_subcribe(message):
-    subcribe[message.from_user.id]=True
+    subcribe[message.from_user.id] = True
     with open("idchats.txt", 'wb') as fichero:
-    	pickle.dump(subcribe, fichero,0)
+        pickle.dump(subcribe, fichero, 0)
 
-@bot.message_handler(commands=['ChangeTimeToCheck']) #func=lambda message: "/ChangeTimeToCheck" in message.text
+
+@bot.message_handler(commands=['ChangeTimeToCheck'])  # func=lambda message: "/ChangeTimeToCheck" in message.text
 def handle_ChangeTimeToCheck(message):
     global time
     try:
@@ -286,22 +310,24 @@ def handle_ChangeTimeToCheck(message):
     except:
         bot.send_message(message.from_user.id, "Error\n The command should be /ChangeTimeToCheck <Time in seconds>")
 
+
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     print message
     bot.send_message(message.from_user.id, "I don't know what you mean.\n" + init_text)
 
-
-
     # bot.send_message(message.from_user.id, "ActiveKeyboard", reply_markup=mainKeyBoard)
 
+
 Timer(1, bot.polling).start()
+
 
 def checkMachines():
     for k, ip in machines.iteritems():
         response = os.system("ping -c 1 " + ip)
         if response is not 0:
             Error(k)
+
 
 def checkSystem():
     global time
@@ -313,18 +339,19 @@ def checkSystem():
             if sensor["id"] in [1, 2, 3, 4]:  # Procesar Sensores Jardin
                 data = doRequest(sensor["id"])
                 if data is not None:
-                    list_days=[]
+                    list_days = []
                     for d in data:
-                        f = d['fecha'].split(",")[1].replace(" GMT","")[1:]
+                        f = d['fecha'].split(",")[1].replace(" GMT", "")[1:]
                         d = datetime.strptime(f, "%d %b %Y %H:%M:%S")
                         list_days.append(d)
                     last_date = max(list_days)
                     now = datetime.now()
-                    if now + timedelta(minutes = -15) > last_date:
+                    if now + timedelta(minutes=-15) > last_date:
                         Error(SENSOR_ERROR_CODE, sensor["id"])
             else:
                 # Procesar AEMET
                 pass
     uploadErrors()
+
 
 checkSystem()
